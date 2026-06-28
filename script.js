130279dfd36bee11228bd5f4fcecc706d8e7b721
gabrielbrandaoborbafeitosa-ops <gabrielbrandaoborbafeitosa@gmail.com>
2026-06-28 18:26:31 -0300

    novo JS
    Áudio

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let lencos = 1000;
+const som = new Audio('som.mp3');
 function lenco() {
     if (lencos == 0) {
         alert('Acabou')
     } else {
         lencos = lencos - 1 ;
     document.getElementById('txtcontador').innerText = lencos;
+    som.play() ;
     }
 }
